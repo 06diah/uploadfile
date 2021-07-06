75c3ef166bcca00b4fb77e1f2822b9f9c6b009b2
06diah <86775940+06diah@users.noreply.github.com>
2021-07-06 08:30:41 -0700

    Add files via upload

---
+++ tes.sh
@@ -0,0 +1,41 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay By Siregar
+echo $ger"Selamat Datang"
+echo
+sleep 2
+echo $yellow" Silahkan pilih tools nya :"
+echo
+echo $cyan"1. ) Stabilkan Jaringan"
+echo
+echo $blue"0. ) Keluar aja !!!"
+echo
+echo $what
+read -p "MAU APA ASU  : " bro
+
+ if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay Siregar
+echo $green"Stabilkan Jaringan by Siregar"
+sleep 2
+ping -s@1000 1.1.1.1
+fi
+
+if  [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echob $yellow
+figlet "Bye"
+echo $whait"To Be Continued"
+sleep 3
+exit
+fi
